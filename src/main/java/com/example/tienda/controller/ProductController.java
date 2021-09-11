dcae0e6dd7072ee7c2c06eccdf1e855b772aea44
Piero <matematicadivina1@gmail.com>
2021-09-11 00:47:37 -0500

    mejoras carritode compras e imagenes producto

--- src/main/java/com/example/tienda/controller/ProductController.java
+++ src/main/java/com/example/tienda/controller/ProductController.java
@@ -21,17 +21,27 @@ public class ProductController {
     @Autowired
     private final ProductService productService;
     private static DecimalFormat decima = new DecimalFormat("0.00");
+    private static String importeDecimal = "0.00";
     List<CarritoCompras> carrito = new ArrayList<>();
     List<Long> ids = new ArrayList<>();
+
+    @GetMapping("/inicio")
+    public String inicio(Model model){
+
+        return "inicio";
+    }
 
    @GetMapping("/")
    public String index(Model model){
        List<Product> list = new ArrayList<>();
-       /*productService.createProduct(new Product(null, ProductType.AUDIO, "Tampones", 12.56, 10,"Son tampones"));
-       productService.createProduct(new Product(null, ProductType.AUDIO, "Audifonos 3M", 13.56, 10,"xds"));
-       productService.createProduct(new Product(null, ProductType.AUDIO, "Orejeras NoiceCancek", 20.6, 20,"Son xd"));
-       productService.createProduct(new Product(null, ProductType.FACIAL, "Facial Shield", 12.56, 10,"Son xdss"));*/
+       /*
+       productService.createProduct(new Product(null, ProductType.AUDIO, "Tapones", 12.56, 10,"Tapones antiruido de alta calidad","https://sodimac.scene7.com/is/image/SodimacPeru/1070061?fmt=jpg&fit=fit,1&wid=420&hei=420"));
+       productService.createProduct(new Product(null, ProductType.AUDIO, "Orejeras 3M", 13.56, 10,"Orejeras que portejen hasta 50 dB","https://sodimac.scene7.com/is/image/SodimacPeru/3706753?fmt=jpg&fit=fit,1&wid=420&hei=420"));
+       productService.createProduct(new Product(null, ProductType.AUDIO, "Orejeras NoiceCancel", 20.6, 20,"Orejaras profesional suprimen hasta 150dB","https://sodimac.scene7.com/is/image/SodimacPeru/1595245?fmt=jpg&fit=fit,1&wid=420&hei=420"));
+       productService.createProduct(new Product(null, ProductType.FACIAL, "Facial Shield", 12.56, 10,"Protector facial de grafeno","https://sodimac.scene7.com/is/image/SodimacPeru/3935094?fmt=jpg&fit=fit,1&wid=420&hei=420"));
+       */
        productService.getAllProducts().forEach(list::add);
+
 
 
        model.addAttribute("products", list);
@@ -54,13 +64,21 @@ public class ProductController {
        return "detailProduct";
    }
    @GetMapping("/carrito")
+   public String carrito(Model model){
+
+       model.addAttribute("listaCarrito", carrito);
+       model.addAttribute("importe", importeDecimal);
+       return "carrito";
+   }
+
+   @PostMapping("/carrito")
     public String showCarrito(@RequestParam String cantidad, @RequestParam String id, Model model){
         int n_cantidad = Integer.parseInt(cantidad);
         Long n_id = Long.parseLong(id);
         Double precioT = productService.findById(n_id).getPrice() * n_cantidad;
 
         double importe = 0;
-        String importeDecimal = "0.00";
+
         if(!ids.contains(n_id)){
             ids.add(n_id);
             carrito.add(new CarritoCompras(n_cantidad,productService.findById(n_id), precioT));
